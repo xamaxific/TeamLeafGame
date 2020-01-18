0684156bf59be0ed1e92390f9add834a6ebb2ae9
Sam Wong <xamaxific@gmail.com>
2020-01-18 02:27:24 +0000

    Began working on prototype. Doggo can bork particle borks.

---
+++ Assets/Scripts/ButtonActionScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonActionScript : MonoBehaviour
+{
+    public KeyCode m_keyboardKey;
+    public UnityEvent m_onKeyDown;
+    public UnityEvent m_onKeyUp;
+    protected bool m_isDown;
+    public void Update () {
+        if ( Input.GetKeyDown( m_keyboardKey ) ) {
+            m_isDown = true;
+            m_onKeyDown.Invoke();
+        } else if ( Input.GetKeyUp( m_keyboardKey ) ) {
+            m_onKeyUp.Invoke();
+            m_isDown = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject m_prefab;
+    [Tooltip("If < 0 then the instantiated object will exist forever")]
+    public float m_waitForDestroy;
+    public void InstantiateObject () {
+        GameObject spawn = Instantiate( m_prefab, transform.position, Quaternion.identity ) as GameObject;
+        if ( m_waitForDestroy >= 0 ) {
+            Destroy( spawn, m_waitForDestroy );
+        }
+
+    }
+}
